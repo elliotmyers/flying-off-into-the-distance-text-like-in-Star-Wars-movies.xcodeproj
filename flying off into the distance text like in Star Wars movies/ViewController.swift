724921830e8293b56718bab7d6a9ed4c238f3277
elliotmyers <elliotmyers47@gmail.com>
2023-04-02 17:43:02 +0300

    Initial Commit

---
+++ flying off into the distance text like in Star Wars movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flying off into the distance text like in Star Wars movies
+//
+//  Created by Raiden Yamato on 02.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
